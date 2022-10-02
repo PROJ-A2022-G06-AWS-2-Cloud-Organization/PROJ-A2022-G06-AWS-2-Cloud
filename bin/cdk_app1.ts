5cbf4682818d2a1e8cbee8bc4f26f55ab7bc5f71
Miikka Mensio <miikka.mensio@tuni.fi>
2022-10-02 13:27:33 +0300

    Refactoring codebase and also enhancing the readability of the code.

--- bin/cdk_app1.ts
+++ bin/cdk_app1.ts
@@ -2,10 +2,17 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { S3stack } from '../lib/S3stack';
+import { linkGitHubStack } from '../lib/linkGitHubStack';
 
+// Create new cdk App
 const app = new cdk.App();
-new S3stack(app, 'S3stack', { 
-  env: { 
-    account: process.env.CDK_DEFAULT_ACCOUNT, 
-    region: process.env.CDK_DEFAULT_REGION 
-}});
+// Add the Stack to the App
+new S3stack(app, 'S3stack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
+/* Encapsulated the Github and AWS linking to an another class. 
+   Here we just instantiate the connection. */
+new linkGitHubStack(app, 'linkGitHubStack');
